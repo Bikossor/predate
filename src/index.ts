8d4b09c0515f924fec551e38fc8cfd8bb0d3a0bd
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2021-10-31 13:22:26 +0100

    Add createDateTime function (#12)
    #7

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types";
 export * from "./combine";
 export * from "./createDate";
+export * from "./createDateTime";
 export * from "./createTime";
 export * from "./getDate";
 export * from "./getTime";
