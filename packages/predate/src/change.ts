e3b2772ac23d7f2fda0f413ea5aac1bade45eb97
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2021-12-26 20:58:44 +0100

    chore: apply prettier format
    #28

--- packages/predate/src/change.ts
+++ packages/predate/src/change.ts
@@ -1,10 +1,8 @@
 import * as predate from "./";
 
-export function change<T extends predate.Date | predate.DateTime | predate.Time>(
-  toChange: T,
-  value: number,
-  key: keyof T
-): T {
+export function change<
+  T extends predate.Date | predate.DateTime | predate.Time
+>(toChange: T, value: number, key: keyof T): T {
   const oldValue = toChange[key] as unknown as number; // Not the best solution but it works as long as the keys of `T` are all `number`s
 
   return { ...toChange, [key]: oldValue + value };
