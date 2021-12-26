309f475ff94f4ebc3dbdc3cd9230b7ba07c46097
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2021-12-26 21:41:09 +0100

    chore: use prettier (#29)
    * chore(deps): install prettier #28
    * chore: configure prettier #28
    * chore: add prettier npm scripts #28
    * chore: apply prettier format #28

--- packages/predate/src/combine.ts
+++ packages/predate/src/combine.ts
@@ -1,5 +1,8 @@
 import * as predate from "./";
 
-export function combine(date: predate.Date, time: predate.Time): predate.DateTime {
+export function combine(
+  date: predate.Date,
+  time: predate.Time
+): predate.DateTime {
   return { ...date, ...time };
 }
