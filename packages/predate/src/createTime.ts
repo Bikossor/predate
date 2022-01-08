3e8a5ba299032e545db81504b9c8d89ebdc48f3d
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-01-08 21:50:11 +0100

    feat: improve createTime
    #36

--- packages/predate/src/createTime.ts
+++ packages/predate/src/createTime.ts
@@ -5,11 +5,46 @@ import { PreTime } from "./types";
  * @param date
  * @returns
  */
-export function createTime(time: {
-  readonly hours: number;
-  readonly minutes: number;
-  readonly seconds?: number;
-  readonly milliseconds?: number;
-}): PreTime {
-  return { ...time };
-}
+export const createTime = {
+  fromJsDate: (jsDate: Date): PreTime => {
+    return {
+      hours: jsDate.getUTCHours(),
+      minutes: jsDate.getUTCMinutes(),
+      seconds: jsDate.getUTCSeconds(),
+      milliseconds: jsDate.getUTCMilliseconds(),
+    };
+  },
+  fromArray: (
+    dateArray: readonly [
+      hours: number,
+      minutes: number,
+      seconds?: number,
+      milliseconds?: number
+    ]
+  ): PreTime => {
+    return {
+      hours: dateArray[0],
+      minutes: dateArray[1],
+      seconds: dateArray[2],
+      milliseconds: dateArray[3],
+    };
+  },
+  fromObject: (dateObject: {
+    readonly hours: number;
+    readonly minutes: number;
+    readonly seconds?: number;
+    readonly milliseconds?: number;
+  }): PreTime => {
+    return {
+      ...dateObject,
+    };
+  },
+  fromParams: (
+    hours: number,
+    minutes: number,
+    seconds?: number,
+    milliseconds?: number
+  ): PreTime => {
+    return { hours, minutes, seconds, milliseconds };
+  },
+};
