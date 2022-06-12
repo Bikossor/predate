6387b0f1111d722679ed27382d6df9676e06648b
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-06-12 17:25:42 +0200

    feat: add `isDateTimeEqual` function (#79)
    * feat: add `isDateTimeEqual` function
    
    #78
    
    * test: add tests for `isDateTimeEqual`
    
    #78
    
    * docs: add docs for `isDateTimeEqual`
    
    #78

---
+++ packages/predate/src/isDateTimeEqual.test.ts
@@ -0,0 +1,25 @@
+/* eslint-disable functional/functional-parameters */
+/* eslint-disable functional/no-return-void */
+/* eslint-disable functional/no-expression-statement */
+import { createDateTime } from "./createDateTime";
+import { isDateTimeEqual } from "./isDateTimeEqual";
+
+describe("isDateTimeEqual", () => {
+  it("should return \"true\" when two equal \"PreDateTime\" are given", () => {
+    const firstDate = createDateTime.fromArray([2022, 0, 1, 12, 15, 30, 60]);
+    const secondDate = createDateTime.fromArray([2022, 0, 1, 12, 15, 30, 60]);
+
+    const actualIsEqual = isDateTimeEqual(firstDate, secondDate);
+
+    expect(actualIsEqual).toBe(true);
+  });
+
+  it("should return \"false\" when two unequal \"PreDateTime\" are given", () => {
+    const firstDate = createDateTime.fromArray([2022, 0, 1, 12, 15, 30, 60]);
+    const secondDate = createDateTime.fromArray([2022, 1, 1, 12, 15, 30, 60]);
+
+    const actualIsEqual = isDateTimeEqual(firstDate, secondDate);
+
+    expect(actualIsEqual).toBe(false);
+  });
+});
